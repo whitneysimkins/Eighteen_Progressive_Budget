4a93b36738b25900ba936080bf3b9e7d9a11d49e
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-04-14 22:41:48 -0600

    mongoose path in server

--- server.js
+++ server.js
@@ -15,10 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
