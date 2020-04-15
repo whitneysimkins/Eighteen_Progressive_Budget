1bf96fcc39b5d50d31ad1aa2f8848fef65644797
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-04-14 19:55:28 -0600

    js for the mongoose

---
+++ models/budget.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const transactionSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: "Enter your transaction note"
+    },
+    value: {
+      type: Number,
+      required: "Enter the amount"
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  }
+);
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+
+module.exports = Transaction;
